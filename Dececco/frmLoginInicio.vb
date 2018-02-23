3b6987a2099538501fbe8fa43c6bd0f5bb97d474
alanmasriera <alan.masriera@gmail.com>
2018-02-23 15:58:39 -0300

    Prueba agragar mensaje

--- Dececco/frmLoginInicio.vb
+++ Dececco/frmLoginInicio.vb
@@ -67,7 +67,6 @@ Public Class frmLoginInicio
     End Sub
 
     Private Sub frmLoginInicio_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        MessageBox.Show("Hola")
         Me.es_admin = Me.verificarPermiso()
 
         Me.Load_tree()
